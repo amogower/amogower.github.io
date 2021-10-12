081aa9ace55d28990a31841e408fbc4ca047d70a
Andrew Gower <andrew.gower@oddschecker.com>
2021-10-12 16:01:14 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+export const App = () => (
+  <div className="grid grid-rows-2 gap-4 justify-center w-screen h-screen bg-black">
+    <h1 className="self-end text-white text-4xl md:text-6xl">amogower.com</h1>
+    <h3 className="self-start text-white text-md md:text-xl">Under construction...</h3>
+  </div>
+);
